069c6d729d24fda3e230d77083731225b4a8c4f2
Muzamal Farooq <muzjani327@gmail.com>
2026-07-04 14:34:51 +0500

    aa

--- components/Header.js
+++ components/Header.js
@@ -19,10 +19,10 @@ export default function Header() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
         {/* Branding Logo */}
         <Link href="/" className="flex items-center gap-2 group cursor-pointer">
-          <div className="p-2 rounded-xl bg-gradient-to-tr from-primary-indigo to-primary-purple text-white shadow-md shadow-indigo-500/20 group-hover:scale-102 transition duration-200">
+          <div className="p-2 rounded-xl bg-linear-to-tr from-primary-indigo to-primary-purple text-white shadow-md shadow-indigo-500/20 group-hover:scale-102 transition duration-200">
             <DownloadCloud className="w-5 h-5" />
           </div>
-          <span className="text-md font-black tracking-tight bg-gradient-to-r from-zinc-900 to-zinc-600 dark:from-zinc-50 dark:to-zinc-400 bg-clip-text text-transparent">
+          <span className="text-md font-black tracking-tight bg-linear-to-r from-zinc-900 to-zinc-600 dark:from-zinc-50 dark:to-zinc-400 bg-clip-text text-transparent">
             VELOCE
           </span>
           <span className="text-[10px] font-black uppercase text-indigo-500 bg-indigo-500/5 dark:bg-indigo-500/10 border border-indigo-500/20 px-1.5 py-0.5 rounded-md">
